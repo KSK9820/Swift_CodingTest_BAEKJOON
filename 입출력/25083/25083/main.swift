6d836c7da1f21e49bcb804b8b535e3fbea84a40f
KSK9820 <68066104+KSK9820@users.noreply.github.com>
2022-05-03 14:13:47 +0900

    입출력

---
+++ 입출력/25083/25083/main.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  25083
+//
+//  Created by 김수경 on 2022/05/03.
+//
+
+import Foundation
+
+print(#"""
+         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |
+"""#)
+
